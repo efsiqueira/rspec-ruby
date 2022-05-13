d877aef9d15735cb4f65967b13416c9631bd4723
efsiqueira <edu.fss14@gmail.com>
2022-05-13 16:07:08 -0300

    Add composicao_spec

--- spec/composicao/composicao_spec.rb
+++ spec/composicao/composicao_spec.rb
@@ -1,8 +1,8 @@
 describe 'Ruby on Rails' do # Utilização de 'and' e 'or' em testes
   it { is_expected.to start_with('Ruby').and end_with('Rails') }
   it { expect(fruta).to eq('banana').or eq('laranja').or eq('uva').or eq('manga').or eq('morango') }
-
-  def fruta
-    %w(banana laranja uva manga morango).sample
-  end
+  
+  # def fruta # helper method arbitrário
+  #   %w(banana laranja uva manga morango).sample
+  # end
 end
