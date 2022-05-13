cdf62631fb5607842bb7151429eaae361ab8d124
efsiqueira <edu.fss14@gmail.com>
2022-05-13 10:34:49 -0300

    Add class Calculator

---
+++ lib/calculator.rb
@@ -0,0 +1,18 @@
+class Calculator
+
+    def sum(first_number, second_number) 
+      result = first_number + second_number
+    end
+
+    def sub(first_number, second_number)
+      result = first_number - second_number
+    end
+
+    def mul(first_number, second_number)
+      result = first_number * second_number
+    end
+
+    def div(first_number, second_number)
+      result = first_number / second_number
+    end
+end
